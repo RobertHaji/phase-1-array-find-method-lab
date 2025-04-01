63b20260ed9f911cde353ef1deefccaa426f3d4f
Robert Haji <roberthadji66@gmail.com>
2025-04-01 13:38:42 +0300

    array-find-lab-done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function superbowlWin(record) {
+  const win = record.find((game) => game.result === "W");
+  return win ? win.year : undefined;
+}
+
+const record1 = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+];
+
+const record2 = [
+  { year: "2012", result: "L" },
+  { year: "2011", result: "N/A" },
+];
+
+test("returns the year of the first Super Bowl win", () => {
+  expect(superbowlWin(record1)).toBe("2015");
+});
+
+test("returns undefined if no Super Bowl win is found", () => {
+  expect(superbowlWin(record2)).toBe(undefined);
+});
